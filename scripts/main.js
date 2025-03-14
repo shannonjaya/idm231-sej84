4114b706347dee42ed62c10d23c6bf9b99708693
Shannon Jaya <sej84@drexel.edu>
2025-03-14 18:18:04 -0400

    ✨ Add zodiac functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const character_data = [ // All character data
+const characterData = [ // All character data
     {
         name: 'Azula', //Aries
         image: 'media/azula.jpeg',
@@ -83,43 +83,7 @@ const character_data = [ // All character data
         sound: 'media/sounds/appa.mp3',
         description: 'Loyal, dependable, protective. Appa is deeply connected to his loved ones and always puts them first. His gentle nature and unwavering loyalty make him an invaluable companion. Though quiet, his presence alone is reassuring.'
     },
-
 ]
-
-const form = document.querySelector('form');
-
-function log_birthday(birthday) { //Logs birthday info
-    const date = {
-        year: birthday[0],
-        month: birthday[1],
-        day: birthday [2]
-    }
-    return date;
-}
-function handle_submit(event) { //Form submission
-    event.preventDefault();
-
-    const date_object = log_birthday(form.elements['birthday'].value.split('-'));
-    console.log(date_object);
-    console.log(date_object.day);
-    console.log(date_object.month);
-    console.log(date_object.year);
-}
-
-if (form) {
-    form.addEventListener('submit', handle_submit);
-}
-
-let all_sounds = [];  // Catches all playing sounds
-
-
-function stop_all_sounds() { // Make all sounds stop
-    all_sounds.forEach((sound) => {
-        sound.pause();
-        sound.currentTime = 0;
-    })
-
-}
 
 const title = document.querySelector('.title');
 const arrow = document.querySelector('.arrow');
@@ -129,13 +93,33 @@ const character_name = document.getElementById('character_name');
 const character_image = document.getElementById('character_image');
 const character_description = document.getElementById('character_description');
 
-function minimize_title() {
+function minimizeTitle() {
     title.classList.add('minimized');
     bday_form.classList.add('active');
+    arrow.style.display = 'none';
+    character_display.style.display = 'none';
+    void character_display.offsetHeight;
+    character_display.style.display = 'flex';
 }
 
-function show_character_details(characterName) {
-    const character = character_data.find(char => char.name === characterName)
+let allSounds = [];  // Catches all playing sounds
+
+function stopSounds() { // Make all sounds stop
+    allSounds.forEach((sound) => {
+        sound.pause();
+        sound.currentTime = 0;
+    })
+}
+
+function playSound(sound) {
+    if (sound) {
+        allSounds.push(sound);
+        sound.play(); 
+    }
+}
+
+function displayCharacter(characterName) {
+    let character = characterData.find(char => char.name === characterName)
 
     if (character) {
         character_name.textContent = character.name;
@@ -143,39 +127,112 @@ function show_character_details(characterName) {
         character_gif.alt = character.name;
         character_description.textContent = character.description;
 
-        minimize_title()
-        arrow.style.display = 'none';
-        character_display.style.display = 'none';
-
-        void character_display.offsetHeight;
-
-        character_display.style.display = 'flex';
+        minimizeTitle()
 
         setTimeout(() => {
             character_display.classList.add('show');
         }, false);
 
+        stopSounds();
+        const characterAudio = new Audio(character.sound);
+        playSound(characterAudio);
+
     } else {
-        console.error("Character not found");
+        console.error("Character not found", characterName);
     }
 }
 
-function create_button(button_name, button_image, sound_file) { // Make character buttons
+const helpButton = document.getElementById('help_button');
+
+if (helpButton) {
+    helpButton.addEventListener('click', () => {
+        character_name.textContent = 'Need help?';
+        character_gif.src = 'media/atla.webp';
+        character_gif.alt = 'Help';
+        character_description.textContent = "Enter your birthday, hit submit, and we'll tell you what character you are from Avatar: The Last Airbender! You'll get to see their name, image, a little about them, and even hear their voice. Click on buttons to check out more characters!";
+
+        minimizeTitle()
+
+        setTimeout(() => {
+            character_display.classList.add('show');
+        }, false);
+
+        stopSounds();
+    }, false);
+}
+
+function getZodiac(month, day) {
+    let character;
+    if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
+      character = 'Iroh';
+    } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
+      character = 'Momo';
+    } else if ((month === 10 && day >= 24) || (month === 11 && day <= 21)) {
+      character = 'Suki';
+    } else if ((month === 9 && day >= 23) || (month === 10 && day <= 23)) {
+      character = 'Aang';
+    } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
+      character = 'Mai';
+    } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
+      character = 'Zuko';
+    } else if ((month === 6 && day >= 22) || (month === 7 && day <= 22)) {
+      character ='Katara';
+    } else if ((month === 5 && day >= 21) || (month === 6 && day <= 21)) {
+      character = 'Ty Lee';
+    } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
+      character = 'Toph';
+    } else if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
+      character = 'Azula';
+    } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
+      character = 'Appa';
+    } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
+      character = 'Sokka';
+    } 
+    displayCharacter(character);
+}
+
+const form = document.querySelector('form');
+
+function logBirthday(birthday) { //Logs birthday info
+    const date = {
+        year: birthday[0],
+        month: birthday[1],
+        day: birthday [2]
+    }
+    
+    return date;
+}
+
+function handleSubmit(event) { //Form submission
+    event.preventDefault();
+  
+    const dateObject = logBirthday(form.elements['birthday'].value.split('-'));
+    const month = parseInt(dateObject.month, 10);
+    const day = parseInt(dateObject.day, 10);
+    console.log(dateObject);
+    console.log(dateObject.day);
+    console.log(dateObject.month);
+    console.log(dateObject.year);
+  
+    getZodiac(month, day);  // Pass month & day to getZodiac()
+}  
+  
+if (form) {
+    form.addEventListener('submit', handleSubmit);
+}
+
+
+  function createButton({name, image}) { // Make character buttons
     const btn = document.createElement('button');
     btn.classList.add('character');
-    btn.id  = button_name.toLowerCase().replace(/\s+/g, '_'); // Sets id lowercase and underscore space
+    btn.id  = name.toLowerCase().replace(/\s+/g, '_'); // Sets id lowercase and underscore space
     
     const img= document.createElement('img'); 
-    img.src = button_image;
-    img.alt= button_name.replace(/([A-Z])/g, ' $1').trim(); 
-
-    const audio = new Audio(sound_file);
-    all_sounds.push(audio);
+    img.src = image;
+    img.alt= name.replace(/([A-Z])/g, ' $1').trim(); 
 
     btn.addEventListener('click', () => {
-        stop_all_sounds();
-        audio.play();
-        show_character_details(button_name);
+        displayCharacter(name);
     });
 
     btn.appendChild(img);
@@ -191,37 +248,10 @@ function initialize() {
         return;
     }
 
-    character_data.forEach((item) => {
-        create_button(item.name, item.image, item.sound);
+    characterData.forEach((character) => {
+        createButton(character);
     });
     }
 
 window.addEventListener('load', initialize);
 
-// function getZodiac(month, day) {
-//     if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
-//       character = 'Iroh';
-//     } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
-//       character = 'Momo';
-//     } else if ((month === 10 && day >= 24) || (month === 11 && day <= 21)) {
-//       character = 'Suki';
-//     } else if ((month === 9 && day >= 23) || (month === 10 && day <= 23)) {
-//       character = 'Aang';
-//     } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
-//       character = 'Mai';
-//     } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
-//       character = 'Zuko';
-//     } else if ((month === 6 && day >= 22) || (month === 7 && day <= 22)) {
-//       character ='Katara';
-//     } else if ((month === 5 && day >= 21) || (month === 6 && day <= 21)) {
-//       character = 'Ty Lee';
-//     } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
-//       character = 'Toph';
-//     } else if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
-//       character = 'Azula';
-//     } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
-//       character = 'Appa';
-//     } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
-//       character = 'Sokka';
-//     } 
-//   }
